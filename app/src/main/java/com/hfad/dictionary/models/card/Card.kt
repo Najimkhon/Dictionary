fbebe696b0612c6e0f006486a10129f4c18d078b
Najimkhon <qwerty1995.290@gmail.com>
2022-12-24 12:35:48 +0500

    Create Card.kt

---
+++ app/src/main/java/com/hfad/dictionary/models/card/Card.kt
@@ -0,0 +1,18 @@
+package com.hfad.dictionary.models.card
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "card_table")
+@Parcelize
+data class Card (
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val word:String,
+    val priority: Status,
+    val definition:String,
+    val example:String
+        ):Parcelable
+
