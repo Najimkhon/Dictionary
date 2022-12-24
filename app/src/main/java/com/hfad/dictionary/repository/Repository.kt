0f4ac884fe8caa01c6dab4a07846f3b948e12a16
Najimkhon <qwerty1995.290@gmail.com>
2022-12-24 12:49:54 +0500

    add room to repository

--- app/src/main/java/com/hfad/dictionary/repository/Repository.kt
+++ app/src/main/java/com/hfad/dictionary/repository/Repository.kt
@@ -1,16 +1,45 @@
 package com.hfad.dictionary.repository
 
+import androidx.lifecycle.LiveData
+import com.hfad.dictionary.ViewModel.CardDAO
 import com.hfad.dictionary.api.RetrofitInstance
 import com.hfad.dictionary.models.api.ExamplesResponse
 import com.hfad.dictionary.models.api.SearchResponse
+import com.hfad.dictionary.models.card.Card
 import retrofit2.Response
 
-class Repository {
+class Repository(private val cardDao: CardDAO) {
+    val getAllData: LiveData<List<Card>> = cardDao.getAllData()
+    val sortByLearned: LiveData<List<Card>> = cardDao.sortByLowLearned()
+    val sortByNew: LiveData<List<Card>> = cardDao.sortByNewStatus()
+    val sortByRepeat: LiveData<List<Card>> = cardDao.sortByRepeat()
+
+    //Api
     suspend fun getDefinition(word: String):Response<SearchResponse> {
         return RetrofitInstance.api.getDefinition(word)
     }
 
     suspend fun getExamples(word: String):Response<ExamplesResponse> {
         return RetrofitInstance.api.getExamples(word)
+    }
+
+    //Room
+    suspend fun insertData(card: Card){
+        cardDao.insertData(card)
+    }
+    suspend fun updateItem(card: Card){
+        cardDao.updateItem(card)
+    }
+
+    suspend fun deleteItem(card: Card){
+        cardDao.deleteItem(card)
+    }
+
+    suspend fun deleteAll(){
+        cardDao.deleteAll()
+    }
+
+    fun searchThroughDatabase(query: String):LiveData<List<Card>>{
+        return cardDao.searchThroughDatabase(query)
     }
 }
