6da837bc7165ad67d8ae53eb943c706502620fdf
Najimkhon <qwerty1995.290@gmail.com>
2022-12-24 12:44:35 +0500

    converter and database class added

---
+++ app/src/main/java/com/hfad/dictionary/ViewModel/CardDatabase.kt
@@ -0,0 +1,37 @@
+package com.hfad.dictionary.ViewModel
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.hfad.dictionary.models.card.Card
+
+
+@Database(entities = [Card::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
+abstract class CardDatabase:RoomDatabase() {
+    abstract fun toDoDao():CardDAO
+
+    companion object{
+        @Volatile
+        private var INSTANCE: CardDatabase? = null
+
+        fun getDatabase(context: Context):CardDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CardDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+
+        }
+    }
+}
