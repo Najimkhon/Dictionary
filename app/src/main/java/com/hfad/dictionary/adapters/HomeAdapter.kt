90a3ba2f0467f5adf6e3741e1f93eb2d44a08499
Najimkhon <qwerty1995.290@gmail.com>
2022-12-26 17:43:38 +0500

    Home RV  and toolbar added

--- app/src/main/java/com/hfad/dictionary/adapters/HomeAdapter.kt
+++ app/src/main/java/com/hfad/dictionary/adapters/HomeAdapter.kt
@@ -2,9 +2,14 @@ package com.hfad.dictionary.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.hfad.dictionary.R
 import com.hfad.dictionary.databinding.CardItemLayoutBinding
+import com.hfad.dictionary.diffutils.CardDiffUtil
 import com.hfad.dictionary.models.card.Card
+import com.hfad.dictionary.models.card.Status
 
 class HomeAdapter(): RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
 
@@ -18,11 +23,39 @@ class HomeAdapter(): RecyclerView.Adapter<HomeAdapter.HomeViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.cardItemLayoutBinding.tvWord.text = mCardList[position].word
+        holder.cardItemLayoutBinding.tvDefinition.text = mCardList[position].definition
+        holder.cardItemLayoutBinding.tvExample.text = mCardList[position].example
+        when (mCardList[position].status) {
+            Status.New -> holder.cardItemLayoutBinding.cvStatus.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.cardItemLayoutBinding.root.context,
+                    R.color.red
+                )
+            )
+            Status.Repeat -> holder.cardItemLayoutBinding.cvStatus.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.cardItemLayoutBinding.root.context,
+                    R.color.yellow
+                )
+            )
+            Status.Learned -> holder.cardItemLayoutBinding.cvStatus.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.cardItemLayoutBinding.root.context,
+                    R.color.green
+                )
+            )
+        }
     }
 
     override fun getItemCount(): Int {
         return mCardList.size
+    }
+    fun setCardData(cardList: List<Card>) {
+        val cardDiffUtil = CardDiffUtil(mCardList, cardList)
+        val cardDiffResult = DiffUtil.calculateDiff(cardDiffUtil)
+        this.mCardList = cardList
+        cardDiffResult.dispatchUpdatesTo(this)
     }
 
     inner class HomeViewHolder(val cardItemLayoutBinding: CardItemLayoutBinding) :
--- app/src/main/java/com/hfad/dictionary/fragments/HomeFragment.kt
+++ app/src/main/java/com/hfad/dictionary/fragments/HomeFragment.kt
@@ -6,15 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.hfad.dictionary.R
+import com.hfad.dictionary.ViewModel.ViewModel
+import com.hfad.dictionary.ViewModelFactory
+import com.hfad.dictionary.adapters.HomeAdapter
+import com.hfad.dictionary.adapters.ListOfCardsAdapter
 import com.hfad.dictionary.databinding.FragmentHomeBinding
+import com.hfad.dictionary.repository.Repository
+import jp.wasabeef.recyclerview.animators.SlideInUpAnimator
 
 
 class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val adapter: HomeAdapter by lazy { HomeAdapter() }
+    private val mViewModel: ViewModel by viewModels() {
+        ViewModelFactory(
+            Repository(),
+            requireActivity().application
+        )
+    }
 
 
     override fun onCreateView(
@@ -31,11 +46,25 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
         binding.tvSeeAllwords.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_wordFragment)
         }
-        binding.btnSeeAll.setOnClickListener{
+        binding.btnSeeAll.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_listFragment)
         }
+        mViewModel.getAllData.observe(viewLifecycleOwner){
+            adapter.setCardData(it)
+        }
+        setupRecyclerView()
+
         return view
 
+
+    }
+
+    private fun setupRecyclerView() {
+        val recyclerView = binding.rvRecents
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager =
+            StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
+        recyclerView.itemAnimator = SlideInUpAnimator().apply { addDuration = 300 }
 
     }
 
