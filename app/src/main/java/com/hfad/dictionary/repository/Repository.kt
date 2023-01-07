f61d857b2c43a5248a6f23c43d05829f6eefaf9e
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2023-01-07 17:52:09 +0500

    Update Repository.kt

--- app/src/main/java/com/hfad/dictionary/repository/Repository.kt
+++ app/src/main/java/com/hfad/dictionary/repository/Repository.kt
@@ -6,11 +6,12 @@ import com.hfad.dictionary.models.api.SearchResponse
 import retrofit2.Response
 
 class Repository {
-    suspend fun getDefinition(word: String):Response<SearchResponse> {
+
+    suspend fun getDefinition(word: String): Response<SearchResponse> {
         return RetrofitInstance.api.getDefinition(word)
     }
 
-    suspend fun getExamples(word: String):Response<ExamplesResponse> {
+    suspend fun getExamples(word: String): Response<ExamplesResponse> {
         return RetrofitInstance.api.getExamples(word)
     }
 
